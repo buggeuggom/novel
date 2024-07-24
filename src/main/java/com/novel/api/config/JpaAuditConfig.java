b165bb5bfd772050d8e3526366466ff888dddb4a
InKyuHwang001 <hwanginkyu42@naver.com>
2024-07-24 14:44:45 +0900

    feat:  EnableJpaAuditing 추가

---
+++ src/main/java/com/novel/api/config/JpaAuditConfig.java
@@ -0,0 +1,7 @@
+package com.novel.api.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
--- src/test/java/com/novel/api/controller/EpisodeControllerTest.java
+++ src/test/java/com/novel/api/controller/EpisodeControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.novel.api.dto.EpisodeDto;
 import com.novel.api.dto.request.episode.WriteEpisodeRequest;
 import com.novel.api.dto.response.PageingResponse;
+import com.novel.api.exception.NovelApplicationException;
 import com.novel.api.fixture.EpisodeFixture;
 import com.novel.api.fixture.TestInfoFixture;
 import com.novel.api.service.EpisodeService;
@@ -19,19 +20,19 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+import static com.novel.api.exception.ErrorCode.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("NovelControllerTest")
+@DisplayName("EpisodeControllerTest")
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class EpisodeControllerTest {
 
@@ -82,6 +83,42 @@ class EpisodeControllerTest {
 
     @Test
     @WithMockUser
+    @DisplayName("[write][fail]: NOVEL_NOT_FOUND")
+    void write_fail_novel_not_found() throws Exception {
+        //given
+        var info = TestInfoFixture.get();
+        WriteEpisodeRequest mockRequest = mock(WriteEpisodeRequest.class);
+        String json = objectMapper.writeValueAsString(mockRequest);
+
+        doThrow(new NovelApplicationException(NOVEL_NOT_FOUND)).when(episodeService).write(eq(info.getNovelId()), any(), any());
+        //expected
+        mockMvc.perform(post("/api/v1/novels/{novelId}/episodes", info.getNovelId())
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().is(NOVEL_NOT_FOUND.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("[write][fail]: INVALID_PERMISSION")
+    void write_fail_invalid_permission() throws Exception {
+        //given
+        var info = TestInfoFixture.get();
+        WriteEpisodeRequest mockRequest = mock(WriteEpisodeRequest.class);
+        String json = objectMapper.writeValueAsString(mockRequest);
+
+        doThrow(new NovelApplicationException(INVALID_PERMISSION)).when(episodeService).write(eq(info.getNovelId()), any(), any());
+        //expected
+        mockMvc.perform(post("/api/v1/novels/{novelId}/episodes", info.getNovelId())
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().is(INVALID_PERMISSION.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
     @DisplayName("[get][success]: ")
     void get_success() throws Exception {
         //given
@@ -95,6 +132,23 @@ class EpisodeControllerTest {
                         .contentType(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("[get][fail]: EPISODE_NOT_FOUND")
+    void get_fail_episode_not_found() throws Exception {
+        //given
+        var info = TestInfoFixture.get();
+
+        //when
+        doThrow(new NovelApplicationException(EPISODE_NOT_FOUND)).when(episodeService).get(eq(info.getEpisodeId()));
+
+        //expected
+        mockMvc.perform(get("/api/v1/episodes/{episodeId}", info.getEpisodeId())
+                        .contentType(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(EPISODE_NOT_FOUND.getStatus().value()));
     }
 
     @Test
